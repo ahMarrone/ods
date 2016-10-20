dda8569fd609dc5184780601878198421421fc77
Your Name <you@example.com>
2016-10-20 14:17:15 -0300

    INSERTS de Objetivos y Metas verdaderos

---
+++ docs/insert_objetivos.sql
@@ -0,0 +1,21 @@
+SET FOREIGN_KEY_CHECKS = 0;
+DELETE FROM objetivos;
+INSERT INTO objetivos (id, descripcion) VALUES (1,'Poner fin a la pobreza en todas sus formas en todo el mundo');
+INSERT INTO objetivos (id, descripcion) VALUES (2,'Poner fin al hambre, lograr la seguridad alimentaria y la mejora de la nutrición y promover la agricultura sostenible');
+INSERT INTO objetivos (id, descripcion) VALUES (3,'Garantizar una vida sana y promover el bienestar para todos en todas las edades');
+INSERT INTO objetivos (id, descripcion) VALUES (4,'Garantizar una educación inclusiva, equitativa y de calidad y promover oportunidades de aprendizaje durante toda la vida para todos');
+INSERT INTO objetivos (id, descripcion) VALUES (5,'Lograr la igualdad entre los géneros y empoderar a todas las mujeres y las niñas');
+INSERT INTO objetivos (id, descripcion) VALUES (6,'Garantizar la disponibilidad de agua y su gestión sostenible y el saneamiento para todos');
+INSERT INTO objetivos (id, descripcion) VALUES (7,'Garantizar el acceso a una energía asequible, segura, sostenible y moderna para todos');
+INSERT INTO objetivos (id, descripcion) VALUES (8,'Promover el crecimiento económico sostenido, inclusivo y sostenible, el empleo pleno y productivo y el trabajo decente para todos');
+INSERT INTO objetivos (id, descripcion) VALUES (9,'Construir infraestructuras resilientes, promover la industrialización inclusiva y sostenible y fomentar la innovación');
+INSERT INTO objetivos (id, descripcion) VALUES (10,'Reducir la desigualdad en y entre los países');
+INSERT INTO objetivos (id, descripcion) VALUES (11,'Lograr que las ciudades y los asentamientos humanos sean inclusivos, seguros, resilientes y sostenibles');
+INSERT INTO objetivos (id, descripcion) VALUES (12,'Garantizar modalidades de consumo y producción sostenibles');
+INSERT INTO objetivos (id, descripcion) VALUES (13,'Adoptar medidas urgentes para combatir el cambio climático y sus efectos');
+INSERT INTO objetivos (id, descripcion) VALUES (14,'Conservar y utilizar en forma sostenible los océanos, los mares y los recursos marinos para el desarrollo sostenible');
+INSERT INTO objetivos (id, descripcion) VALUES (15,'Proteger, restablecer y promover el uso sostenible de los ecosistemas terrestres, gestionar los bosques de forma sostenible de los bosques, luchar contra la desertificación, detener e invertir la degradación de las tierras y poner freno a la pérdida de la diversidad biológica');
+INSERT INTO objetivos (id, descripcion) VALUES (16,'Promover sociedades pacíficas e inclusivas para el desarrollo sostenible, facilitar el acceso a la justicia para todos y crear instituciones eficaces, responsables e inclusivas a todos los niveles');
+INSERT INTO objetivos (id, descripcion) VALUES (17,'Fortalecer los medios de ejecución y revitalizar la Alianza Mundial para el Desarrollo Sostenible Finanzas');
+
+SET FOREIGN_KEY_CHECKS = 1;
