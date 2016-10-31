15deb32a9dd30fec6866230b2407bd3f10394ac5
earissola <esteban.rissola@gmail.com>
2016-10-31 20:23:45 -0300

    Actualizacipón 'Explora'

---
+++ dev/web/data.js
@@ -0,0 +1,103 @@
+{
+    "Provincia":
+    {
+        "0":{
+            "2014": {"pobreza_h": 10, "pobreza_m": 90},
+            "2015": {"pobreza_h": 90, "pobreza_m": 10}
+            },
+        "1":{
+            "2014": {"pobreza_h": 80, "pobreza_m": 10},
+            "2015": {"pobreza_h": 10, "pobreza_m": 80}
+            },
+        "2":{
+            "2014": {"pobreza_h": 40, "pobreza_m": 90},
+            "2015": {"pobreza_h": 90, "pobreza_m": 40}
+        },
+        "3":{
+            "2014": {"pobreza_h": 70, "pobreza_m": 25},
+            "2015": {"pobreza_h": 25, "pobreza_m": 75}
+        },
+        "4":{
+            "2014": {"pobreza_h": 75, "pobreza_m": 35},
+            "2015": {"pobreza_h": 35, "pobreza_m": 75}
+        },
+        "5":{
+            "2014": {"pobreza_h": 30, "pobreza_m": 70},
+            "2015": {"pobreza_h": 70, "pobreza_m": 30}
+        },
+        "6":{
+            "2014": {"pobreza_h": 60, "pobreza_m": 5},
+            "2015": {"pobreza_h": 5, "pobreza_m": 60}
+        },
+        "7":{
+            "2014": {"pobreza_h": 50, "pobreza_m": 80},
+            "2015": {"pobreza_h": 80, "pobreza_m": 50}
+        },
+        "8":{
+            "2014": {"pobreza_h": 2, "pobreza_m": 68},
+            "2015": {"pobreza_h": 68, "pobreza_m": 2}
+        },
+        "9":{
+            "2014": {"pobreza_h": 90, "pobreza_m": 10},
+            "2015": {"pobreza_h": 10, "pobreza_m": 90}
+        },
+        "10":{
+            "2014": {"pobreza_h": 25, "pobreza_m": 80},
+            "2015": {"pobreza_h": 80, "pobreza_m": 25}
+        },
+        "11":{
+            "2014": {"pobreza_h": 15, "pobreza_m": 60},
+            "2015": {"pobreza_h": 60, "pobreza_m": 15}
+        },
+        "12":{
+            "2014": {"pobreza_h": 43, "pobreza_m": 15},
+            "2015": {"pobreza_h": 15, "pobreza_m": 43}
+        },
+        "13":{
+            "2014": {"pobreza_h": 67, "pobreza_m": 80},
+            "2015": {"pobreza_h": 80, "pobreza_m": 67}
+        },
+        "14":{
+            "2014": {"pobreza_h": 9, "pobreza_m": 98},
+            "2015": {"pobreza_h": 98, "pobreza_m": 9}
+        },
+        "15":{
+            "2014": {"pobreza_h": 10, "pobreza_m": 65},
+            "2015": {"pobreza_h": 65, "pobreza_m": 10}
+        },
+        "16":{
+            "2014": {"pobreza_h": 24, "pobreza_m": 70},
+            "2015": {"pobreza_h": 70, "pobreza_m": 24}
+        },
+        "17":{
+            "2014": {"pobreza_h": 69, "pobreza_m": 10},
+            "2015": {"pobreza_h": 10, "pobreza_m": 69}
+        },
+        "18":{
+            "2014": {"pobreza_h": 72, "pobreza_m": 23},
+            "2015": {"pobreza_h": 23, "pobreza_m": 72}
+        },
+        "19":{
+            "2014": {"pobreza_h": 89, "pobreza_m": 7},
+            "2015": {"pobreza_h": 50, "pobreza_m": 89}
+        },
+        "20":{
+            "2014": {"pobreza_h": 95, "pobreza_m": 20},
+            "2015": {"pobreza_h": 20, "pobreza_m": 95}
+        },
+        "21":{
+            "2014": {"pobreza_h": 20, "pobreza_m": 87},
+            "2015": {"pobreza_h": 87, "pobreza_m": 20}
+        },
+        "22":{
+            "2014": {"pobreza_h": 76, "pobreza_m": 8},
+            "2015": {"pobreza_h": 8, "pobreza_m": 76}
+        }
+    },
+    "Nación":{
+        "23":{
+         "2014": {"pobreza_h": 5, "pobreza_m": 95},
+         "2015": {"pobreza_h": 95, "pobreza_m": 5}              
+        }
+    }        
+}
