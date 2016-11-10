085b6e96701e118c99cb13a524b03674e42a1fdc
agustin <agustinhmarrone@gmail.com>
2016-11-10 13:50:42 -0300

    Objeto ThreeSelect.js agrega campo 'name' a los selects del formulario

--- dev/web/js/ThreeSelect.js
+++ dev/web/js/ThreeSelect.js
@@ -3,33 +3,35 @@
 var template_select = [
 	'<div class="form-group">',
 	'<label>Objetivos</label>',
-    '<select class="form-control selectOne">',
+    '<select id="three_select_objetivo" class="form-control selectOne" name="id_objetivo_selected">',
     '<% _.each(model.get("objetivos"), function( objetivo, i){ %>',
     '<option value="<%= objetivo.id %>" <% if (objetivo.id == model.get("objetivo_selected")) { %> selected<% }  %>><%= objetivo.desc %></option>',
 	'<% }); %>',
     '</select>',
     '</div>',
-    '<div class="form-group">',
-	'<label>Metas</label>',
-    '<select id="indicadores_fkidmeta" class="form-control selectTwo" name="indicadores[fkidmeta]">',
-    '<% _.each(model.get("metas"), function( meta, i){ %>',
-    '<% if ( meta.id_objetivo == model.get("objetivo_selected") ){ %>',
-    '<option value="<%= meta.id %>" <% if (meta.id == model.get("meta_selected")) { %> selected<% }  %>><%= meta.desc %></option>',
-    '<% } %>',
-	'<% }); %>',
-    '</select>',
-    '</div>',
-    '<% if (model.get("indicadores").length ){ %>',
-    '<div class="form-group">',
-	'<label>Indicadores</label>',
-    '<select class="form-control selectThree">',
-    '<% _.each(model.get("indicadores"), function( indicador, i){ %>',
-    '<% if ( indicador.id_meta == model.get("meta_selected") ){ %>',
-    '<option value="<%= indicador.id %>" <% if (indicador.id == model.get("indicador_selected")) { %> selected<% }  %>><%= indicador.desc %></option>',
-    '<% } %>',
-	'<% }); %>',
-    '</select>',
-    '</div>',
+    '<% if (model.get("metas").length ){ %>',
+        '<div class="form-group">',
+    	'<label>Metas</label>',
+        '<select id="three_select_meta" class="form-control selectTwo" name="id_meta_selected">',
+        '<% _.each(model.get("metas"), function( meta, i){ %>',
+        '<% if ( meta.id_objetivo == model.get("objetivo_selected") ){ %>',
+        '<option value="<%= meta.id %>" <% if (meta.id == model.get("meta_selected")) { %> selected<% }  %>><%= meta.desc %></option>',
+        '<% } %>',
+    	'<% }); %>',
+        '</select>',
+        '</div>',
+        '<% if (model.get("indicadores").length ){ %>',
+        '<div class="form-group">',
+    	'<label>Indicadores</label>',
+        '<select id="three_select_indicador" class="form-control selectThree" name="id_indicador_selected">',
+        '<% _.each(model.get("indicadores"), function( indicador, i){ %>',
+        '<% if ( indicador.id_meta == model.get("meta_selected") ){ %>',
+        '<option value="<%= indicador.id %>" <% if (indicador.id == model.get("indicador_selected")) { %> selected<% }  %>><%= indicador.desc %></option>',
+        '<% } %>',
+    	'<% }); %>',
+        '</select>',
+        '</div>',
+        '<% } %>',
     '<% } %>',
 ].join("\n");
 
@@ -48,7 +50,9 @@ var ThreeSelectData = Backbone.Model.extend({
 var ThreeSelectView = Backbone.View.extend({
 	initialize: function() {
 		this.model.set('objetivo_selected',this.model.get('objetivos')[0].id);
-		this.model.set('meta_selected',this.model.get('metas')[0].id);
+        if (this.model.get('metas').length) {
+            this.model.set('meta_selected',this.model.get('metas')[0].id);
+        }
         if (this.model.get('indicadores').length) {
             this.model.set('indicador_selected',this.model.get('indicadores')[0].id);
         }
