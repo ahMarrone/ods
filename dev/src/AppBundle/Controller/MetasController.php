f69ee98b8fd9dde71f80665a378d593da0414216
agustin <agustinhmarrone@gmail.com>
2017-01-06 01:24:15 -0300

    Añado cambios en pantallas de Metas

--- dev/src/AppBundle/Controller/MetasController.php
+++ dev/src/AppBundle/Controller/MetasController.php
@@ -9,6 +9,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Entity\Metas;
 use AppBundle\Form\MetasType;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 /**
  * Metas controller.
  *
@@ -27,8 +29,10 @@ class MetasController extends Controller
     {
         $id_objetivo = $request->get('id_objetivo');
         $em = $this->getDoctrine()->getManager();
-        
-        if (intval($id_objetivo) == 0)
+        $objetivos = $this->getObjetivosPreload();
+        $metas = $em->getRepository('AppBundle:Metas')->findAll();
+        $objetivoSelected = (intval($id_objetivo) == 0) ? $objetivos[0]['id'] : intval($id_objetivo);
+        /*if (intval($id_objetivo) == 0)
         {
             $metas = $em->getRepository('AppBundle:Metas')->findAll();
             $titulo_objetivo = "TODOS";
@@ -39,12 +43,23 @@ class MetasController extends Controller
             $objetivo = $em->getRepository('AppBundle:Objetivos')->findOneById($id_objetivo);
             $titulo_objetivo = $objetivo->getDescripcion();
             $metas = $em->getRepository('AppBundle:Metas')->findByfkidobjetivo($id_objetivo);            
-        }
+        }*/
 
         return $this->render('metas/index.html.twig', array(
             'metas' => $metas,
-            'titulo_objetivo' => $titulo_objetivo,
+            'titulo_objetivo' => "titulo objetivo",
+            'objetivos' => $objetivos,
+            'objetivo_selected' => $objetivoSelected,
         ));
+    }
+
+    private function getObjetivosPreload(){
+        $list = array();
+        $objetivos =  $this->getDoctrine()->getRepository('AppBundle:Objetivos')->findAll();
+        foreach ($objetivos as $o) {
+            array_push($list, array('id'=>$o->getId(),'desc'=>$o->getId() ." - " . $o->getDescripcion()));
+        }
+        return $list;
     }
 
     /**
@@ -57,6 +72,7 @@ class MetasController extends Controller
     {
         $meta = new Metas();
         $form = $this->createForm('AppBundle\Form\MetasType', $meta, array(
+            'entity_manager' => $this->getDoctrine()->getManager(),
             'scopes_enabled' => $this->getEnabledScopes(),
         ));
         $form->handleRequest($request);
@@ -73,6 +89,7 @@ class MetasController extends Controller
         return $this->render('metas/new.html.twig', array(
             'meta' => $meta,
             'form' => $form->createView(),
+            'api_urls' => array('get_next_meta_code'=> $this->generateUrl('admin_crud_metas_get_next_meta_code'))
         ));
     }
 
@@ -90,6 +107,35 @@ class MetasController extends Controller
             'meta' => $meta,
             'delete_form' => $deleteForm->createView(),
         ));
+    }
+
+
+    /**
+     *
+     * @Route("/api/get_next_meta_code", name="admin_crud_metas_get_next_meta_code")
+     * @Method("POST")
+     */
+    public function getNextMetaCodeAction(Request $request)
+    {
+        $content = $this->get("request")->getContent();
+        if (!empty($content)){
+            $data = json_decode($content, true);
+            $response = $this->getMetaNextCode($data['objetivo_id']);
+            return new JsonResponse($response);
+        }
+    }
+
+    private function getMetaNextCode($idObjetivo){
+        $em = $this->getDoctrine()->getManager();
+        $highest_id = $em->createQueryBuilder()
+            ->select('MAX(e.codigo)')
+            ->from('AppBundle:Metas', 'e')
+            ->where('e.fkidobjetivo = ?1')
+            ->setParameter(1, $idObjetivo)
+            ->getQuery()
+            ->getSingleScalarResult();
+        $highest_id = $highest_id + 1;
+        return array("next_meta_code" => $highest_id);
     }
 
     /**
