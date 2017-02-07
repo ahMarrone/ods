08cd19a101cb83b59d62a5048c7ee9172a9640ba
agustin <agustinhmarrone@gmail.com>
2017-02-07 13:58:41 -0300

    Creo jerarquia de usuario. NUEVO USUARIO ROOT. Root puede registrar usuarios

---
+++ dev/src/AppUserBundle/AppUserBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppUserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AppUserBundle extends Bundle
+{
+
+	public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
+    
+}
