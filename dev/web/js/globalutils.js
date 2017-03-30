085df8e91d08db1131ab382d794b0eae71e83e1b
agustin <agustinhmarrone@gmail.com>
2017-03-30 14:50:12 -0300

    Pantalla aprobacion muestra cartel con resultado de operacion

--- dev/web/js/globalutils.js
+++ dev/web/js/globalutils.js
@@ -1,17 +1,26 @@
 
-function showSimpleModal(modalID, messageType, message){
+function showSimpleModal(modalID, messageType, message, acceptCallback, acceptMsgButton){
     var icon = "";
     var fullMessage = "";
     var modalObject = $('#'+ modalID);
     var modalBody = modalObject.find('.modal-body');
+    var acceptMsg = (acceptMsgButton) ? acceptMsgButton : "Aceptar";
+    var modalOptions = {}
     switch(messageType){
       case 'success':  icon = '<i class="fa fa-check" style="color: green"></i>';
                        break;
       case 'error' :   icon = '<i class="fa fa-times" style="color: red"></i>';
     }
     var fullMessage = icon + "  " + message;
+    if (acceptCallback){
+      modalOptions = {backdrop: "static", keyboard: "false"};
+      fullMessage += '<div class="modal-footer"><button id="acceptSimpleModal" type="button" class="btn btn-'+messageType+'">'+acceptMsg+'</button></div>';
+    }
     modalBody.html(fullMessage);
-    modalObject.modal('show');
+    modalObject.modal(modalOptions);
+    $('#'+modalID).on('click', '#acceptSimpleModal',function(){
+        acceptCallback();
+    });
 }
 
 function appendSpinner(domElement){
